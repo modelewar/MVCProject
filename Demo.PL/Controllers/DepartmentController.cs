323412091a8f887e4b2fe068c57e81fdd6071b46
MO Delewar <delewarmohamed@gmail.com>
2024-10-09 10:26:47 +0300

    Part09-DepartmentController Edit

--- Demo.PL/Controllers/DepartmentController.cs
+++ Demo.PL/Controllers/DepartmentController.cs
@@ -38,19 +38,49 @@ namespace Demo.PL.Controllers
             return View(department);
         }
 
-        public IActionResult Details(int? id)
+        public IActionResult Details(int? id , string ViewName = "Details" )
         {
             if (id is null)
-            {
-                return BadRequest();
-            }//Status Code 400
+              return BadRequest();//Status Code 400
             var department = _departmentRepository.GetById(id.Value);
             if (department is null)
-            {
                 return NotFound();
+            return View(ViewName,department);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (id is null)
+            //    return BadRequest();
+
+            //var department = _departmentRepository.GetById(id.Value);
+            //if (department is null)
+            //    return NotFound();
+            //return View(department);
+            return Details(id , "Edit");
+
+        }
+
+        public IActionResult Edit(Department department , [FromRoute] int id )
+        {
+            if (id != department.Id)
+                return BadRequest();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _departmentRepository.Update(department);
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (System.Exception ex)
+                {
+
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
 
             }
-            return View(department);
+                return View(department);
         }
     }
 }
