8384b6402421c1cfac58ddadccf32e8b721418f6
MO Delewar <delewarmohamed@gmail.com>
2024-10-10 14:50:30 +0300

    Employee Repository

---
+++ Demo.BLL/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        public IEnumerable<Employee> GetAll();
+        Employee GetById(int id);
+        int Add(Employee  employee);
+        int Update(Employee employee);
+        int Delete(Employee employee);
+    }
+}
---
+++ Demo.BLL/Repositories/EmployeeRepository.cs
@@ -0,0 +1,48 @@
+﻿using Demo.BLL.Interfaces;
+using Demo.DAL.Contexts;
+using Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Repositories
+{
+    internal class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly MVCDbContext _dbConytext;
+        public EmployeeRepository(MVCDbContext dbConytext)
+        {
+            _dbConytext = dbConytext;
+        }
+        public int Add(Employee employee)
+        {
+            _dbConytext.Add(employee);
+           return _dbConytext.SaveChanges();
+        }
+
+        public int Delete(Employee employee)
+        {
+             _dbConytext.Remove(employee);
+            return _dbConytext.SaveChanges();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        => _dbConytext.Employees.ToList();
+
+
+
+
+        public Employee GetById(int id)
+        => _dbConytext.Employees.Find(id);
+            
+      
+
+        public int Update(Employee employee)
+        {
+           _dbConytext.Employees.Update(employee);
+            return _dbConytext.SaveChanges();
+        }
+    }
+}
